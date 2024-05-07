7ca3cc6ddf38e3f80b79083fe81565da2e517692
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2024-05-07 15:05:56 -0500

    Initial commit

---
+++ onlinePython.py
@@ -0,0 +1,17 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+
+raise "Something went wrong"  # Noncompliant: a string is not a valid exception
+
+def fun(a):
+  i = 10
+  return i + a       # Noncompliant
+  i += 1             # this is never executed
